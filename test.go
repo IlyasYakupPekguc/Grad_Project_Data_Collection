f41ed27b0b4717073353c6a3bcae70a52a1772f0
Zeyn28 <zeynepaygun06@gmail.com>
2025-01-25 17:23:37 +0300

    new file added

---
+++ test.go
@@ -0,0 +1,24 @@
+// Golang program to illustrate the usage of
+// fmt.print() function
+
+// Including the main package
+package main
+
+// Importing fmt
+import (
+	"fmt"
+)
+
+// Calling main
+func main() {
+
+	// Declaring some const variables
+	const num1, num2, num3 = 5, 15, 15
+
+	// Calling print() function
+	fmt.Print(num1, num2, num3, "\n")
+
+	// It is conventional not to worry about any
+	// error returned by Print.
+
+}
